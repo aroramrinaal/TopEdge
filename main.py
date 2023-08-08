d30f28e59e07a75aa8a34307f2af5043b316e2ec
aroramrinaal <apply.mrinaal@gmail.com>
2023-08-08 13:41:34 +0530

    add more functionality to the cpu command.

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ def main():
 
 @app.command()
 def cpu():
-    cpu_percent = psutil.cpu_percent(interval=1)
-    typer.echo(f"CPU Usage: {cpu_percent}%")
+    cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
+    num_cores = psutil.cpu_count(logical=False)
+
+    typer.echo("CPU Usage:")
+    for core, percent in enumerate(cpu_percent, start=1):
+        typer.echo(f"  Core {core}: {percent}%")
+    
+    typer.echo(f"Total CPU Cores: {num_cores}")
+
 
 
 if __name__ == "__main__":
