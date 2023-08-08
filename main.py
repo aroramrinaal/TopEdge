65ab838e8378cf4198004dc0368a64ee169debad
aroramrinaal <apply.mrinaal@gmail.com>
2023-08-08 13:28:17 +0530

    initializing the psutil module.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import typer
+import psutil
 
 app = typer.Typer()
 
 @app.command()
 def main():
     typer.echo("Welcome to TopEdge - The Modern CLI System Monitor")
+
+@app.command()
+def cpu():
+    cpu_percent = psutil.cpu_percent(interval=1)
+    typer.echo(f"CPU Usage: {cpu_percent}%")
+
 
 if __name__ == "__main__":
     app()
